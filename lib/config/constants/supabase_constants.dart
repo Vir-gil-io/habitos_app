6b40b311ec1c4577142ae224a7d8cb6a23079e42
Brayan Daniel Mediola Gutierrez <116902837+Brayanpower@users.noreply.github.com>
2026-07-15 13:11:39 -0600

    conexion con supabase}

---
+++ lib/config/constants/supabase_constants.dart
@@ -0,0 +1,5 @@
+class SupabaseConstants {
+  static const String url = 'https://mvxydkthlwkkocwihzlm.supabase.co';
+  static const String publishableKey =
+      'sb_publishable_PreOgat8IYv3NdeKW4jKgA_GEFsEj2y';
+}
